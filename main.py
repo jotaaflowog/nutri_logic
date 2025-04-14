47e621c02cf88297e12e5250f1dfa0dc10e6ce16
jotaaflowog <jorgebutto1107@gmail.com>
2025-04-14 12:49:30 -0400

    Estructura base, carpetas backend/frontend, entorno virtual y archivos de proyecto

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+def main():
+    print("🧮 Bienvenido a NUTRI CALCULO")
+    print("----------------------------")
+
+    # Aquí irá el flujo principal del programa
+    # 1. Solicitar datos del paciente
+    # 2. Elegir fórmula para calcular GET
+    # 3. Ajustar calorías
+    # 4. Calcular macronutrientes
+    # 5. Mostrar resultados
+
+    # Por ahora, solo un mensaje temporal
+    print("Módulo en construcción...")
+
+
+if __name__ == '__main__':
+    main()
+
+
